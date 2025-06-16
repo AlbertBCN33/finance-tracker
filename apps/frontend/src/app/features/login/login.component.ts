82d1cf2f6d19fe39045d8f26d84836c2e3e2bbdc
albert33 <albert_haf@hotmail.com>
2025-06-16 10:52:23 +0200

    Updated Login

--- apps/frontend/src/app/features/login/login.component.ts
+++ apps/frontend/src/app/features/login/login.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslatePipe } from '@ngx-translate/core';
+import { AuthService } from '@finance-tracker/data-access';
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule],
+  imports: [CommonModule, TranslatePipe, ButtonModule, DividerModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  authService = inject(AuthService);
+  logoSrc = 'img/logo-horizontal-light.svg';
+  googleIconSrc = 'img/google-icon-logo.svg';
+}
--- apps/frontend/src/app/features/main/main.routes.ts
+++ apps/frontend/src/app/features/main/main.routes.ts
@@ -5,5 +5,35 @@ export const routes: Route[] = [
   {
     path: '',
     component: MainComponent,
+    children: [
+      {
+        path: 'index-funds',
+        loadChildren: () =>
+          import('../../features/index-funds/index-funds.routes').then(
+            (m) => m.routes || console.error('Error loading index-funds')
+          ),
+      },
+      {
+        path: 'stocks',
+        loadChildren: () =>
+          import('../../features/stocks/stocks.routes').then(
+            (m) => m.routes || console.error('Error loading stocks')
+          ),
+      },
+      {
+        path: 'whishlists',
+        loadChildren: () =>
+          import('../../features/whishlists/whishlists.routes').then(
+            (m) => m.routes || console.error('Error loading whishlists')
+          ),
+      },
+      {
+        path: 'news',
+        loadChildren: () =>
+          import('../../features/news/news.routes').then(
+            (m) => m.routes || console.error('Error loading news')
+          ),
+      },
+    ],
   },
 ];
